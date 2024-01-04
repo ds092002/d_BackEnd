ea02da4e2ea88732cfd4be960f2d81f7a48c8787
Dhaval Solanki <dhavalas24@gmail.com>
2024-01-04 16:19:33 +0530

    4 Jan

--- d_MySQL/04-01-24.sql
+++ d_MySQL/04-01-24.sql
@@ -13,3 +13,24 @@ select * from customers where country = 'USA' and city = 'NYC' and customerNumbe
 select * from customers where country = 'USA' or customerNumber > 400;
 
 select * from customers where country in ('USA','Germany','france');
+
+select * from customers where country not in ('USA','Germany','france','UK');
+
+select * from customers where customerNumber >= 200 and customerNumber <= 350;
+
+select * from customers where customerNumber between 200 and 400;
+
+select * from customers where customerNumber not between 200 and 400;
+
+select * from customers where customerName like 'm%';
+select * from customers where customerName like 'mini%';
+select * from customers where customerName like 'd%';
+select * from customers where customerName like 's%'and country = 'USA';
+select * from customers where customerName like '%d%' and addressLine1 = 7;
+select * from customers where customerName like 'd%s';
+select * from customers where customerName like 'd%d';
+select * from customers where customerName like 'd%' and contactFirstName like 'm%';
+select * from customers where customerName like '%o_';
+select * from customers where customerName like '_o%___' and addressLine2 like 'p%';
+
+
