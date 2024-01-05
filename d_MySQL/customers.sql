63a740905e79c7019631d21afd42d653bca86916
Dhaval Solanki <dhavalas24@gmail.com>
2024-01-05 16:15:25 +0530

    5 Jan

---
+++ d_MySQL/customers.sql
@@ -0,0 +1,68 @@
+use classicmodels;
+
+select * from customers;
+
+select * from customers limit 7;
+
+select * from customers where country = 'USA';
+
+select * from customers where country = 'USA' and city = 'NYC';
+
+select * from customers where country = 'USA' and city = 'NYC' and customerNumber > 400;
+
+select * from customers where country = 'USA' or customerNumber > 400;
+
+select * from customers where country in ('USA','Germany','france');
+
+select * from customers where country not in ('USA','Germany','france','UK');
+
+select * from customers where customerNumber >= 200 and customerNumber <= 350;
+
+select * from customers where customerNumber between 200 and 400;
+
+select * from customers where customerNumber not between 200 and 400;
+
+select * from customers where customerName like 'm%';
+select * from customers where customerName like 'mini%';
+select * from customers where customerName like 'd%';
+select * from customers where customerName like 's%'and country = 'USA';
+select * from customers where customerName like '%d%' and addressLine1 = 7;
+select * from customers where customerName like 'd%s';
+select * from customers where customerName like 'd%d';
+select * from customers where customerName like 'd%' and contactFirstName like 'm%';
+select * from customers where customerName like '%o_';
+select * from customers where customerName like '_o%___' and addressLine2 like 'p%';
+
+
+select country from customers group by country;
+select distinct creditLimit from customers;
+select country , count(customerNumber) from customers group by country order by country;
+
+select max(creditLimit) from customers;
+select min(creditLimit) from customers;
+select sum(creditLimit) from customers;
+select avg(creditLimit) from customers;
+
+select country, max(creditLimit) from customers group by country;
+select country, min(creditLimit) from customers group by country;
+select country, min(creditLimit) from customers group by country order by country;
+select country, sum(creditLimit) from customers group by country;
+select country, avg(creditLimit) from customers group by country;
+
+
+
+
+select * from customers where country not in ('USA');       -- (4) 
+select * from customers where customerNumber not between 150 and 200;   -- (5)
+select * from customers where creditLimit > 70000 order by contactLastName desc;     --  (7)
+select * from customers where city in ('Singapore','Liverpool','NYC');     --  (8) 
+select * from customers where contactLastName like 'Smith%';     -- (9) 
+select * from customers where creditLimit between 50000 and 95000;    --  (11) 
+select * from customers where contactLastName like 'S%';   --  (13) 
+select * from customers where contactLastName like 'S%h';    --  (14) 
+select * from customers where contactLastName like 's____';     --  (15) 
+select * from customers where contactLastName like '________';    --  (16)  
+select * from customers where customerName like 'a%e';      --  (17) 
+-- select * from customers where customerName like 'ee%';    --   (18) 
+select state, max(creditLimit) from customers group by state;  --  (20) 
+select state from customers group by state;
--- d_MySQL/employe.sql
+++ d_MySQL/employe.sql
@@ -1,12 +1,14 @@
 
-select * from employees;
+select * from employees;     -- (1) 
 
-select * from employees where jobTitle = 'Sales Rep' and employeeNumber Between 1200 and 1500;
+select * from employees where jobTitle = 'Sales Rep' and employeeNumber Between 1200 and 1500;      -- (2) 
 
-select * from employees where officeCode >= 2 and jobTitle not in  ('Sales Rep');
+select * from employees where officeCode >= 2 and jobTitle not in  ('Sales Rep');      -- (3) 
 
-select firstName , lastName as  'Name Of The Employee' from employees order by lastname;
+select firstName , lastName as  'Name Of The Employee' from employees order by lastname;     -- (6) 
 
-select * from employees where officeCode in (1,3,5);
+select * from employees where officeCode in (1,3,5);    -- (10) 
 
-select * from employees where officeCode not in (1,3);
+select * from employees where officeCode not in (1,3);     -- (12)
+
+select jobTitle , count(*) from employees group by jobTitle;    --   (19) 
