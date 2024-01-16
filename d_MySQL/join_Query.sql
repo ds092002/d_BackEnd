20423aa8cba153729c69b4ec866a9400168f0725
Dhaval Solanki <dhavalas24@gmail.com>
2024-01-16 12:51:17 +0530

    16 Jan

--- d_MySQL/join_Query.sql
+++ d_MySQL/join_Query.sql
@@ -5,7 +5,7 @@ show tables;
 select * from client_master;
 select * from product_master;
 select * from salesman_master;
-select * from sales_order where orderstatus like 'ful%';
+select * from sales_order ;
 select * from sales_orderDetail;
 
 
@@ -20,17 +20,37 @@ select so.*, cm.Cname, cm.city from sales_order so Right join client_master cm o
 
 
 select sod.*, cm.Cname, pm.Description from sales_orderDetail sod
-inner join product_master pm on pm.productno = sod.productno
 inner join sales_order so on so.OrderNo = sod.OrderNo
-inner join client_master cm on so.ClientNo = cm.ClientNo;
+inner join client_master cm on so.ClientNo = cm.ClientNo
+inner join product_master pm on sod.productno =  pm.productno where CName = "Ivan Bayross";        -- 1
 
-select sod.*, cm.*, pm.* from sales_orderDetail sod
-inner join product_master pm on pm.productno = sod.productno
+SELECT sod.ProductNo, pm.Description, sod.QtyOrdered, sod.QtyDisp, so.OrderDate
+FROM sales_order so
+JOIN sales_orderDetail sod ON so.OrderNo = sod.OrderNo
+JOIN product_master pm ON sod.ProductNo = pm.ProductNo
+WHERE MONTH(so.DelyDate) = MONTH(CURRENT_DATE()) AND YEAR(so.DelyDate) = YEAR(CURRENT_DATE());     -- 2
+
+select distinct pm.ProductNo , pm.Description from sales_orderdetail sod 
+inner join product_master pm on pm.ProductNo = sod.productNo;         -- 3
+
+select sod.*, cm.Cname, pm.ProductNo , pm.Description from sales_orderDetail sod
 inner join sales_order so on so.OrderNo = sod.OrderNo
-inner join client_master cm on so.ClientNo = cm.ClientNo  where CName like '%Ivan Bayross';
+inner join client_master cm on so.ClientNo = cm.ClientNo
+inner join product_master pm on sod.productno =  pm.productno where Description = "Trousers";    -- 4
 
-select sod.*, cm.*, pm.* from sales_orderDetail sod
-inner join product_master pm on pm.productno = sod.productno
+SELECT so.OrderNo, c.CName, p.Description, sod.QtyOrdered
+FROM sales_orderDetail sod
+JOIN sales_order so ON sod.OrderNo = so.OrderNo
+JOIN client_master c ON so.ClientNo = c.ClientNo
+JOIN product_master p ON sod.ProductNo = p.ProductNo
+WHERE p.Description = 'Pull Overs' AND sod.QtyOrdered < 5;      -- 5
+
+select sod.*, cm.Cname, pm.Description from sales_orderDetail sod
 inner join sales_order so on so.OrderNo = sod.OrderNo
-inner join client_master cm on so.ClientNo = cm.ClientNo  where Description like 'tro%';
+inner join client_master cm on so.ClientNo = cm.ClientNo
+inner join product_master pm on sod.productno =  pm.productno where cm.CName in ('Ivan Bayross','Mamta Muzumdar');      -- 6
 
+select sod.*, cm.Cname, pm.Description from sales_orderDetail sod
+inner join sales_order so on so.OrderNo = sod.OrderNo
+inner join client_master cm on so.ClientNo = cm.ClientNo
+inner join product_master pm on sod.productno =  pm.productno where cm.ClientNo in ('C00001','C00002');      -- 7
