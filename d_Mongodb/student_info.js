810328d3492520cc8b1473bb67269cf1c1746210
Dhaval Solanki <dhavalas24@gmail.com>
2024-01-22 17:47:54 +0530

    22 Jan

---
+++ d_Mongodb/student_info.js
@@ -0,0 +1,162 @@
+db.students.insertmany([
+    {
+        FirstName: 'Dhaval',
+        LastName: 'Solanki',
+        Gender: 'Male',
+        Age: 21,
+        Email: 'dd@gmail.com',
+        Address: {
+            Address1: 'A-18 , Haridham Soc;',
+            Address2: 'Near Archana School',
+            City: 'Surat',
+            State: 'Gujarat',
+            Country: 'India',
+            Pincode: 395010
+        },
+        Subjects: {
+            Maths: 99,
+            Science: 99,
+            English: 99
+        } 
+    },
+    {
+        FirstName: 'Vijay',
+        LastName: 'Solanki',
+        Gender: 'Male',
+        Age: 22,
+        Email: 'Vijay@gmail.com',
+        Address: {
+            Address1: '518 , Matrushakti Soc;',
+            Address2: 'Punagam Road',
+            City: 'Surat',  
+            State: 'Gujarat',   
+            Country: 'India',   
+            Pincode: 395010
+        },
+        Subjects: {
+            Maths: 99,
+            Science: 99,
+            English: 99
+        }
+    },
+    {
+        FirstName: 'Darshan',
+        LastName: 'Chauhan',
+        Gender: 'Male',
+        Age: 18,
+        Email: 'darshan@gmail.com',
+        Address: {
+            Address1: 'C-9 , RaviPark Soc;',
+            Address2: 'Punagam Road',
+            City: 'Surat',
+            State: 'Gujarat',
+            Country: 'India',
+            Pincode: 395010
+        },
+        Subjects: {
+            Maths: 99,
+            Science: 99,
+            English: 99
+        }
+    },
+    {
+        FirstName: 'Jenil',
+        LastName: 'Kakadiya',
+        Gender: 'Male',
+        Age: 18,
+        Email: 'jenil@gmail.com',
+        Address: {
+            Address1: '54 Ayodhya Nagar',
+            Address2: 'Yogi Chowk',
+            City: 'Surat',
+            State: 'Gujarat',
+            Country: 'India',
+            Pincode: 395010
+        },
+        Subjects: {
+            Maths: 99,
+            Science: 99,
+            English: 99
+        }
+    },
+    {
+        FirstName: 'Krish',
+        LastName: 'Ranghani',
+        Gender: 'Male',
+        Age: 18,
+        Email: 'krish@gmail.com',
+        Address: {
+            Address1: '32 Ashok Vatika',
+            Address2: 'Nana Varchha',
+            City: 'Surat',
+            State: 'Gujarat',
+            Country: 'India',
+            Pincode: 395010
+        },
+        Subjects: {
+            Maths: 99,
+            Science: 99,
+            English: 99
+        }
+    },
+    {
+        FirstName: 'Bhautik',
+        LastName: 'Dudhat',
+        Gender: 'Male',
+        Age: 18,
+        Email: 'bhautik@gmail.com',
+        Address: {
+            Address1: '204 Pushp Van',
+            Address2: 'Mota Varchha',
+            City: 'Surat',
+            State: 'Gujarat',
+            Country: 'India',
+            Pincode: 395010
+        },
+        Subjects: {
+            Maths: 99,
+            Science: 99,
+            English: 99   
+        }
+    },
+    {
+        FirstName: 'Mahendra',
+        LastName: 'Purohit',
+        Gender: 'Male',
+        Age: 18,
+        Email: 'mahendra@gmail.com',
+        Address: {
+            Address1: '456 Sarita Vihar',
+            Address2: 'Puna - Bombay Road',
+            City: 'Surat',
+            State: 'Gujarat',
+            Country: 'India',
+            Pincode: 395010
+        },
+        Subjects:{
+            Maths: 99,
+            Science: 99,
+            English: 99
+        }
+    },
+    {
+        FirstName: 'Raj',
+        LastName: 'Kumar',
+        Gender: 'Male',
+        Age: 18,
+        Email: 'raj@gmail.com',
+        Address: {
+            Address1: '1234567890',
+            Address2: 'Puna - Bombay Road',
+            City: 'Surat',
+            State: 'Gujarat',
+            Country: 'India',
+            Pincode: 395010
+        },
+        Subjects:{
+            Maths: 99,
+            Science: 99,
+            English: 99
+        }
+    },
+])
