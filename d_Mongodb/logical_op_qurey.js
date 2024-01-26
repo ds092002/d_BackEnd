f40859633e9d57e38a84b84470273768a05ed4a2
Dhaval Solanki <dhavalas24@gmail.com>
2024-01-26 16:12:06 +0530

    26 Jan

--- d_Mongodb/logical_op_qurey.js
+++ d_Mongodb/logical_op_qurey.js
@@ -1,4 +1,6 @@
 // db.books.find()
 
 // db.books.find({$and: [{"pages" : {$eq: 384}},{"year" : 1350}]});
-db.books.find({$not: [{"pages" : {$eq: 384}}]});
+// db.books.find({$or: [{"pages" : {$eq: 384}},{"year" :{$in: [1610,1835,1952]}}]});
+// db.books.find({$nor: [{"pages" : {$eq: 384}},{"year" :{$in: [1610,1835,1952]}}]});
+db.books.find({"pages" : {$not: {$gte: 384}}});
