8fd73c129b341239acc17906cda6986ec269050a
Dhaval Solanki <dhavalas24@gmail.com>
2024-01-31 19:28:39 +0530

    31 Jan

---
+++ d_Mongodb/forprac2.js
@@ -0,0 +1,75 @@
+// db.createCollection("customers");
+// db.customers.insertOne({
+//     customer_id: "C01",
+//     first_name: "Dhaval",
+//     last_name: "Solanki",
+//     email: "dhaval@gmail.com",
+//     password: 123456
+// });
+// db.customers.insertMany([
+//     {
+//         customer_id: "C02",
+//         first_name: "Vijay",
+//         last_name: "Solanki",
+//         email: "vijay@gmail.com",
+//         password: 123456
+//     },
+//     {
+//         customer_id: "C03",
+//         first_name: "Darshan",
+//         last_name: "Chuahan",
+//         email: "darshan@gmail.com",
+//         password: 123456
+//     },
+//     {
+//         customer_id: "C04",
+//         first_name: "Jenil",
+//         last_name: "Kakadiya",
+//         email: "jenil@gmail.com",
+//         password: 123456
+//     }
+// ]);
+
+// db.createCollection("products")
+// db.products.insertOne({
+//     product_id: "P001",
+//     product_name: "Jeans",
+//     product_description: "Puma",
+//     product_price: 299,
+//     product_quantity: 3,
+//     category: "Category 1"
+// });
+
+// db.products.insertMany([
+//     {
+//         product_id: "P002",
+//         product_name: "Galaxy S23 FE",
+//         product_description: "Samsung",
+//         product_price: 4850,
+//         product_quantity: 3,
+//         category: "Category 1"
+//     },
+//     {
+//         product_id: "P003",
+//         product_name: "Shoes",
+//         product_description: "Nike",
+//         product_price: 100,
+//         product_quantity: 4,
+//         category: "Category 2"
+//     },
+//     {
+//         product_id: "P004",
+//         product_name: "Q-LED",
+//         product_description: "Samsung",
+//         product_price: 600,
+//         product_quantity: 6,
+//         category: "Category 3"
+//     },
+//     {
+//         product_id: "P005",
+//         product_name: "Bullet-Bike",
+//         product_description: "Royal Enfield",
+//         product_price: 3000,
+//         category: "Category 1"
+//     }
+// ]);
