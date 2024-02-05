f15f3bffad7fb68ecec9a184066bb6952fc6b1cf
Dhaval Solanki <dhavalas24@gmail.com>
2024-02-05 15:25:22 +0530

    5 Feb

--- Java Script/clock.js
+++ Java Script/clock.js
@@ -1,16 +1,12 @@
-// Calling showTime function at every second
 setInterval(showTime, 1000);
 
-// Defining showTime funcion
 function showTime() {
-	// Getting current time and date
 	let time = new Date();
 	let hour = time.getHours();
 	let min = time.getMinutes();
 	let sec = time.getSeconds();
 	am_pm = "AM";
 
-	// Setting time for 12 Hrs format
 	if (hour >= 12) {
 		if (hour > 12) hour -= 12;
 		am_pm = "PM";
@@ -29,8 +25,7 @@ hour < 10 ? "0" + hour : hour;
 		":" +
 		sec +
 		am_pm;
-
-	// Displaying the time
+		
 	document.getElementById(
 		"clock"
 	).innerHTML = currentTime;
