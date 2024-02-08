276997f7152e9861e2bb0314bd920ff7a2b0cd4e
Dhaval Solanki <dhavalas24@gmail.com>
2024-02-08 15:39:12 +0530

    8 Feb

--- TypeScript/basic.js
+++ TypeScript/basic.js
@@ -1,2 +1,12 @@
-var a = 7;
-console.log(a);
+// let a = 7;
+// console.log(a);
+
+
+// let a : string[] = ["Hello"];
+// let a : Array<string> = ["Hello", "World"];
+// a[1] = "World";
+// a[2] = "007";
+// a[20] = "101";
+// console.log(a);
+
+ 
