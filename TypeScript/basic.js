5713157216dff4552a4b215bf2fa4dc2c584d72f
Dhaval Solanki <dhavalas24@gmail.com>
2024-02-08 20:16:13 +0530

    8 Feb

--- TypeScript/basic.js
+++ TypeScript/basic.js
@@ -1,8 +1,40 @@
 // let a = 7;
 // console.log(a);
-var a = ["Hello"];
+
+
+// let a : string[] = ["Hello"];
 // let a : Array<string> = ["Hello", "World"];
-a[1] = "World";
-a[2] = "007";
-a[20] = "101";
-console.log(a);
+// a[1] = "World";
+// a[2] = "007";
+// a[20] = "101";
+// console.log(a);
+
+// let a : number = 777;
+// console.log(a);
+
+// let b = "Hello";
+// console.log(b);
+
+// let x : number | string = 101;
+// // x = "Hello";
+// console.log(x);
+
+// let d : string[] = ["Hello","Dhaval"];
+// // let d : Array<string> = ["Hello","Dhaval","Solanki"];
+// d[19] = "DD";
+// console.log(d);
+
+// let n : number[] = [1,2,3,4,5];
+// // let n : Array<Number> = [6,7,8,9,10,11,12,13,14];
+// n[20] = 777;
+// console.log(n);
+
+var x;
+x = ["D", 9, 3, "G"];
+x.push("S", 7);
+x.push(7, "Hello");
+x.push("World", 2002);
+console.log(x);
+// console.log(x.length);
+// console.log(x.pop());
+
--- TypeScript/basic.ts
+++ TypeScript/basic.ts
@@ -9,7 +9,32 @@
 // a[20] = "101";
 // console.log(a);
 
-<<<<<<< HEAD
-=======
- 
->>>>>>> e2fc22b922fac760dd2cc0783ce76173bb21f2d1
+// let a : number = 777;
+// console.log(a);
+
+// let b = "Hello";
+// console.log(b);
+
+// let x : number | string = 101;
+// // x = "Hello";
+// console.log(x);
+
+// let d : string[] = ["Hello","Dhaval"];
+// // let d : Array<string> = ["Hello","Dhaval","Solanki"];
+// d[19] = "DD";
+// console.log(d);
+
+// let n : number[] = [1,2,3,4,5];
+// // let n : Array<Number> = [6,7,8,9,10,11,12,13,14];
+// n[20] = 777;
+// console.log(n);
+
+let x : [string, number, number, string];
+x = ["D", 9 , 3 , "G" ];
+x.push("S",7);
+x.push(7,"Hello");
+x.push("World",2002);
+// console.log(x);
+// console.log(x.length);
+// console.log(x.pop());
+
