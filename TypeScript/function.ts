85c45c99b8db9b2982bee3bebc78992d3a2ce751
Dhaval Solanki <dhavalas24@gmail.com>
2024-02-10 18:49:30 +0530

    10 Feb

--- TypeScript/function.ts
+++ TypeScript/function.ts
@@ -21,10 +21,73 @@
 
 // console.log(person);
 
-enum gender{
-    'male','female','hello'
-}
+// enum gender{
+//     'male','female','hello'
+// }
 
-let x = gender.male;
+// let x = gender.male;
 
-console.log(x);
+// console.log(x);
+
+// function Add(a,b) {
+//     console.log(a + b);
+// }
+// Add(7,'D');    
+    
+// function Add(a:number,b:number,c:number) {
+//     return a * b * c;
+// };
+// let a = Add(1,21,10);
+// console.log(a);
+
+
+// const person = function(a : number, b : string, c : number){
+//     return a + b + c;
+// };
+// console.log(7, 'person' , 7);
+
+
+// let skill = function() {
+//     console.log("SkillQode - Decode youe skill");
+// };
+// skill();    
+
+
+// let x = function(a : number, b : any):any
+// {return a + b;};
+// let z = x(7,'D');
+// console.log(z);
+
+
+// (function(a: number, b: number, c: number, d: number, e: number, f: number, g: number){
+//     console.log("Value of  : ", a, b, c, d, e, f, g);
+//     let x = "Hello ! I called my self";
+//     console.log(x);
+// })(1,2,3,4,5,6,7);
+
+// const myFunction = new Function("a","b","return a + b");
+// let x = myFunction(3,4);
+// console.log(x);
+
+// let x = (x : number,y : number) => x * y;
+// console.log(x(7,1));
+
+// function myFunction(x : number, y : number){
+//     return x + y;
+// };
+// let text = myFunction(2,5);
+// console.log(text);
+
+// function myFunction(x : number, y : number) {
+//     return x + y;
+// };
+// console.log(myFunction.toString(7,1));
+
+
+// function sum(...args: any[]): number{
+//     let sum = 0;
+//     for(let i = 0; i < args.length; i++)
+//             sum *= args[i];
+//     return sum;    
+// }
+// console.log(sum(1,2,3,4,5,6,7,8,9,10,11));
