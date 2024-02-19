fedc20715beb748022ac2f59ec290dcb953275df
Dhaval Solanki <dhavalas24@gmail.com>
2024-02-19 17:41:41 +0530

    19 Feb

--- NodeJS/server2.js
+++ NodeJS/server2.js
@@ -1,14 +1,28 @@
 const http = require('http');
 const fs = require('fs');
-const data = fs.readFileSync('../d_Mongodb/books.json', 'utf8')
+const data = fs.readFileSync('./hello.json', 'utf8');
 
-// Server 2
-const server2 = http.createServer((req, res)=>{
+// const server = http.createServer((req, res)=>{
     // res.end('Welcome to Server 2');
-    res.setHeader('Dummy','Dummy Data')
-    res.end(data)
-    res.end();
+    // res.setHeader('Dummy','Dummy Data')
+    // res.setHeader('content-type','application/json');
+    // res.end(data);
+    // res.end();
+// });
+
+const server = http.createServer();
+server.on('request',(req, res)=>{
+//     res.write('<h1 style="color:yellow">Hello World!</h1>');
+//     res.write('Content-type','text/html)
+//     res.end();
 });
-server2.listen(7000,()=>{
+
+server.on('request',(req,res)=>{
+    console.log(req.url);
+    // res.setHeader('content-type', 'text/html');
+    res.end(data);
+})
+
+server.listen(7000,()=>{
     console.log('Server Start at port 7000');
 });
