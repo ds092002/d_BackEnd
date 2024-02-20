3b492f82072daf86add48cd497ca95a865d96cbc
Dhaval Solanki <dhavalas24@gmail.com>
2024-02-20 09:57:59 +0530

    20 Feb

--- NodeJS/server3.js
+++ NodeJS/server3.js
@@ -1,12 +1,35 @@
-
-
-
 const http = require('http');
+const fs = require('fs');
 
-// Server 3
-const server3 = http.createServer((req, res)=>{
-    res.end('Welcome to Server 3');
+const data = fs.readFileSync('./hello.json', 'utf-8');
+const port = 7001;
+
+const app = http.createServer((req, res) => {
+    console.log(req.url);
+
+    switch (req.url) {
+        case '/':
+            res.setHeader('Content-Type', 'text/html');
+            res.write('<h1 style="color:yellow">Welcome to NodeJs</h1>');
+            res.end();
+            break;
+        case '/product':
+            res.setHeader('Content-Type', 'application/json');
+            res.end(data);
+            break;
+        case '/user':
+            res.setHeader('Content-Type', 'text/html');
+            res.write('<h1 style="color:orange">Welcome to users</h1>');
+            res.end();
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.write('<h1 style="color:red">404 Not Found!</h1>');
+            res.end();
+            break;
+    }
 });
-server3.listen(7001,()=>{
-    console.log('Server Start at port 7001');
-});    
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
