9815c0bda9f28e76d14f605c67c0b8b6ffcc5c5a
Dhaval Solanki <dhavalas24@gmail.com>
2024-02-23 09:59:58 +0530

    23 Feb

--- express/server.js
+++ express/server.js
@@ -1,20 +1,25 @@
 const express = require('express');
-const server = express();
+const server = express();     // server created using Express library.
+const path = require('path');   // used to resolve the file paths for static files.
+
+// Set up the server to serve static files from the 'public' folder.
 
 const port = 7000;
 
-server.get('/',(req,res)=>{
-    res.end('Welcome To Local Server');
-});
-server.get('/product',(req,res)=>{
-    res.end('Welcome To Product Page');
-});
-server.get('/user',(req,res)=>{
-    res.end('Welcome To User Page');
-});
-server.get('/test',(req,res)=>{
-    res.end('Welcome To Test Page');
-});
+// server.get('/',(req,res)=>{
+//     res.end('Welcome To Local Server');
+// });
+// server.get('/product',(req,res)=>{
+//     res.end('Welcome To Product Page');
+// });
+// server.get('/user',(req,res)=>{
+//     res.end('Welcome To User Page');
+// });
+// server.get('/test',(req,res)=>{
+//     res.end('Welcome To Test Page');
+// });
+
+server.post('/')
 
 server.listen(port,()=>{
     console.log( `Server is running at http://localhost:${port}`);
