8ae79e1f78af1d11fc80db7082f80d819ee0362b
Dhaval Solanki <dhavalas24@gmail.com>
2024-02-26 15:58:25 +0530

    26 Feb

--- express/app.js
+++ express/app.js
@@ -15,10 +15,12 @@ let myFun = (req, res, next) => {
 }
 
 // app.use(myFun);
-app.use(express.json())
-// app.use(morgan("dev")); // development server for logging requests tomorgan server instead  of the console 
+app.use(express.json()); // for parsing application/json responses from express server and sending them to express server with express middleware function
+app.use(morgan("dev")); // development server for logging requests tomorgan server instead  of the console 
 // app.use(morgan("combined")) //combined middle handler will log all requests and responses tomorgan and send them tomorgan middle handler with response headers and body
 // app.use(morgan("common")) // common is the default one used when combined option is not specified in config file.
+// app.use(morgan("short")); // short middle handler will log all requests and responses tomorgan and send them to the stream.
+// app.use(morgan("tiny")); // tiny middle handler will log all requests and responses tomorgan and send them to the stream.
 app.get('/',myFun,(req,res)=>{
     res.send('Welcome To Express JS');
 });
