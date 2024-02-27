0f087d8f05ba8bb6834142c7fe4116259f1a0d50
Dhaval Solanki <dhavalas24@gmail.com>
2024-02-27 17:15:26 +0530

    27 Feb

--- express/built_in.js
+++ express/built_in.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 7001;
-// const path = require('path');
+const path = require('path');
 const morgan = require('morgan');
 
 let myFun = (req, res, next) => {
@@ -13,10 +13,17 @@ let myFun = (req, res, next) => {
 }
 
 app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use('/hello',express.static(path.join(__dirname,'public','mahi.jpg')));
+app.use('/mahi2',express.static(path.join(__dirname,'public','mahi2.jpg')));
+app.use('/mahi3',express.static(path.join(__dirname,'public','mahi3.jpg')));
+app.use('/video',express.static(path.join(__dirname,'public','videos.mp4')));
+app.use('/panda',express.static(path.join(__dirname,'public','panda.mp4')));
+app.use('/song',express.static(path.join(__dirname,'public','song.mp3')));
 app.use(morgan('dev'));
 
 app.get('/',myFun,(req,res)=>{
-    res.send('Welcome to the Express JS');
+    res.send('index');
 });
 app.post('/',(req,res)=>{
     console.log(req.body);
