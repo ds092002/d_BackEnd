6a08a0b05d7c33f74618c1f93cf7a5902b0e6714
Dhaval Solanki <dhavalas24@gmail.com>
2024-02-28 17:47:39 +0530

    28 Feb

--- express/app2.js
+++ express/app2.js
@@ -18,8 +18,29 @@ app.post('/products',(req,res)=>{
     // product.push({...req.body});
     res.status(201).json({message : 'Product Added Successfully....'})
 });
+// app.post('/products', (req, res) => {
+//     // console.log(req.body);
+//     const product = req.body;
+//     if (product) {
+//         products.push(product);
+//         res.status(201).json({ message: 'Product Added Successfully....' });
+//     } else {
+//         res.status(404).json({ message: 'Product Not Added ...' });
+//     }
+// });
+
 app.get('/products',(req,res)=>{
-    res.status(201).json(products)
+    res.status(200).json(products)
+});
+
+// app.get('/products,:id',(req,res)=>{
+//     const id = +req.params.id;
+app.get('/products/single-product',(req,res)=>{
+    const id = +req.query.id;
+    // console.log(id);
+    // res.status(201).json(products.find(product => product.id === id));
+    let product = products.find((item)=> item.id === id);
+    res.status(200).json(product);
 });
 
 app.listen(port,()=>{
