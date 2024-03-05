6fdcc1ac90869c5bac73efdcc60b2fe1bb9979e1
Dhaval Solanki <dhavalas24@gmail.com>
2024-03-05 18:52:37 +0530

    5 March

--- express/controller/users2.controll.js
+++ express/controller/users2.controll.js
@@ -1,6 +1,6 @@
 const User = require('../model/user.model');
 
-exports.addUser = async (req,res) => {
+exports.addUsers = async (req, res) => {
     try{
         const {firstName, lastName, gender, email, password, age} = req.body;
         console.log(req.body);
@@ -15,7 +15,61 @@ exports.addUser = async (req,res) => {
         newUser.save();
         res.status(201).json({user : newUser, message : 'New User Is Added...'});
     }catch(err){
-        console.log(err);
+        console.log(error);
         res.status(500).json({message : 'Internal Server Error...'});
     }
-}
+};
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        let users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message : `Internal Server Error...` });
+    }
+};
+
+exports.getUser = async (req, res) => {
+    try {
+        let userId = req.query.userId;
+        let user = await User.findById(userId);
+        if(!user){
+            return res.status(404).json({ message : `User Not Found...` });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message : `Internal Server Error...` });
+    }
+};
+
+exports.updateUser = async (req, res) => {
+    try {
+        let userId = req.query.userId;
+        let user = await User.findById(userId);
+        if(!user){
+            return res.status(404).json({ message : `User Not Found...`});
+        }
+        user = await User.findOneAndUpdate({_id:user._id}, { $set: { ...req.body}}, { new : true });
+        res.status(200).json({user, message : `User updated...`});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message : `Internal Server Error: ${error.message}` });
+    }
+};
+
+exports.deleteUser = async (req, res) => {
+    try {
+        let userId = req.query.userId;
+        let user =  await User.findById(userId);
+        if(!user){
+            return res.status(404).json({ message : `User Not Found...` });
+        }
+        user = await User.findOneAndDelete({_id:user._id});
+        res.status(200).json({ user, message : `User Deleted...` });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message : `Internal Server Error: ${error.message}` });
+    }
+};
--- express/routers/users2.routes.js
+++ express/routers/users2.routes.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const userRoutes = express.Router();
-const {addUser} = require('../controller/users2.controll');
+const { addUsers, 
+        getAllUsers, 
+        getUser, 
+        updateUser, 
+        deleteUser
+} = require('../controller/users2.controll');
 
-userRoutes.post('/add-user', addUser);
+userRoutes.post('/add-user', addUsers);
+
+userRoutes.get('/getAll-users', getAllUsers);
+
+userRoutes.get('/get-users', getUser);
+
+userRoutes.put('/update-user', updateUser);
+
+userRoutes.delete('/delete-user',deleteUser);
 
 module.exports = userRoutes;
