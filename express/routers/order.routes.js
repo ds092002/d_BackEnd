72548ca468d78fab1137fa13e787d496b8de8dae
Dhaval Solanki <dhavalas24@gmail.com>
2024-03-15 17:34:45 +0530

    15 March

--- express/routers/order.routes.js
+++ express/routers/order.routes.js
@@ -2,9 +2,18 @@ const express = require('express');
 const orderRoutes = express.Router();
 const { verifyToken } = require('../helpers/verifyToken');
 const {
-    newOrder
+    newOrder,
+    getAllOrders,
+    getOrder,
+    deleteOrder
 } = require('../controller/order.controller');
 
 orderRoutes.post('/add-order', verifyToken, newOrder);
+
+orderRoutes.get('/getAll-order', verifyToken, getAllOrders);
+
+orderRoutes.get('/get-order', verifyToken, getOrder);
+
+orderRoutes.delete('/delete-order', verifyToken, deleteOrder);
 
 module.exports = orderRoutes;
